23cf104ea80d57348ab176defe0b77f5741cc196
Francis Celada <119907392+Kicoop@users.noreply.github.com>
2023-03-04 09:04:57 +0800

    Add files via upload

---
+++ src/Enemy.java
@@ -0,0 +1,9 @@
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+public abstract class Enemy {
+ public abstract void update();
+ public abstract void draw(Graphics g);
+ public abstract Rectangle getBound();
+ public abstract boolean isOutOfScreen();
+}
